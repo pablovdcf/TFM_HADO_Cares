ed52ac6379fdaabe17b20fe3bb7ccbe911646ec3
pablovdcf <pablovdcf@gmail.com>
2023-06-12 14:59:47 +0200

    Continuando preprocessing y añadidas dos pipelines

---
+++ hado-cares/src/hado_cares/pipelines/data_processing/nodes.py
@@ -0,0 +1,4 @@
+"""
+This is a boilerplate pipeline 'data_processing'
+generated using Kedro 0.18.7
+"""
---
+++ hado-cares/src/hado_cares/pipelines/data_processing/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'data_processing'
+generated using Kedro 0.18.7
+"""
+
+from kedro.pipeline import Pipeline, node, pipeline
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])
